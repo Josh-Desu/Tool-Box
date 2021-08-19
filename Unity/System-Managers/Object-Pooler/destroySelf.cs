4235378187a01855d035068d2d41cb5f24b96c87
ogfeng-josh <joyang112@gmail.com>
2021-08-18 23:56:26 -0700

    Updated object pooling script

--- Unity/System-Managers/Object-Pooler/destroySelf.cs
+++ Unity/System-Managers/Object-Pooler/destroySelf.cs
@@ -11,26 +11,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class destroySelf : MonoBehaviour
+public class destroySelf : MonoBehaviour, IPooledObject
 {
     [Header("Destruction Settings:")]
 
     public float timer = 1.0f;
     public bool disableObject = false;
     public bool destroyObject = false;
-    
+
+    private bool startScript = false;
     private bool readyState = false;
 
     private void Awake()
     {
         //readyState = false;
     }
-    private void Start()
+    public void OnObjectSpawn()
     {
         StartCoroutine(startTimer());
     }
     private void Update()
     {
+        //if(startScript == false)
+        //{
+        //    StartCoroutine(startTimer());
+        
+        //}
+
         checkLoad();
     }
     //Destroy gameobject in scene to keep low load
@@ -40,10 +47,12 @@ public class destroySelf : MonoBehaviour
         if(disableObject == true && readyState == true)
         {
             gameObject.SetActive(false);
+            readyState = false;
         }
         else if(destroyObject == true && readyState == true)
         {
             Destroy(gameObject);
+            readyState = false;
         }
         else if(disableObject == true && destroyObject == true)
         {
@@ -55,6 +64,7 @@ public class destroySelf : MonoBehaviour
     //StartCoroutine(MethodName());
     IEnumerator startTimer()
     {
+        //startScript = true;
         yield return new WaitForSeconds(timer);
         readyState = true;
     }
